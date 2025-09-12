2e1f194ada08a5ebe96996f20119dfcb826b39ad
Gae24 <96017547+Gae24@users.noreply.github.com>
2025-09-12 19:12:22 +0200

    android: rework implementation
    don't store context inside clipboard, adapt code to the latest jni changes

--- src/platform/android.rs
+++ src/platform/android.rs
@@ -1,8 +1,12 @@
-use std::borrow::Cow;
+use std::{
+	borrow::Cow,
+	path::{Path, PathBuf},
+};
 
 use jni::{
 	objects::{JObject, JString},
-	AttachGuard, JavaVM,
+	strings::JNIString,
+	Env, JavaVM,
 };
 
 use crate::{Error, ImageData};
@@ -13,40 +17,36 @@ impl From<jni::errors::Error> for Error {
 	}
 }
 
-pub(crate) struct Clipboard {
-	ctx: ndk_context::AndroidContext,
+fn function<F, T>(callback: F) -> Result<T, Error>
+where
+	F: FnOnce(&mut Env, JObject) -> Result<T, Error>,
+{
+	let ctx = ndk_context::android_context();
+
+	let jvm = unsafe { JavaVM::from_raw(ctx.vm().cast()) };
+
+	jvm.attach_current_thread(|env| {
+		let context = unsafe { JObject::from_raw(ctx.context().cast()) };
+		let clipboard = env.new_string(c"clipboard")?;
+
+		let clipboard_manager = env
+			.call_method(
+				context,
+				c"getSystemService",
+				c"(Ljava/lang/String;)Ljava/lang/Object;",
+				&[(&clipboard).into()],
+			)?
+			.l()?;
+
+		callback(env, clipboard_manager)
+	})
 }
+
+pub(crate) struct Clipboard(());
 
 impl Clipboard {
 	pub(crate) fn new() -> Result<Self, Error> {
-		Ok(Self { ctx: ndk_context::android_context() })
-	}
-
-	fn vm(&self) -> Result<JavaVM, jni::errors::Error> {
-		// SAFETY: Valid pointer guaranteed by the `ndk_context` crate.
-		unsafe { jni::JavaVM::from_raw(self.ctx.vm().cast()) }
-	}
-
-	fn context(&self) -> JObject {
-		// SAFETY: Valid pointer guaranteed by the `ndk_context` crate.
-		unsafe { JObject::from_raw(self.ctx.context().cast()) }
-	}
-
-	fn clipboard_manager<'attachment>(
-		&self,
-		env: &mut AttachGuard<'attachment>,
-	) -> Result<JObject<'attachment>, Error> {
-		let context = self.context();
-		let clipboard = env.new_string("clipboard")?;
-
-		Ok(env
-			.call_method(
-				context,
-				"getSystemService",
-				"(Ljava/lang/String;)Ljava/lang/Object;",
-				&[(&clipboard).into()],
-			)?
-			.l()?)
+		Ok(Self(()))
 	}
 }
 
@@ -60,33 +60,50 @@ impl<'clipboard> Get<'clipboard> {
 	}
 
 	pub(crate) fn text(self) -> Result<String, Error> {
-		let vm = self.clipboard.vm()?;
-		let mut env = vm.attach_current_thread()?;
-		let clipboard_manager = self.clipboard.clipboard_manager(&mut env)?;
+		function(|env, clipboard_manager| {
+			if !env.call_method(&clipboard_manager, c"hasPrimaryClip", c"()Z", &[])?.z()? {
+				return Err(Error::ContentNotAvailable);
+			}
 
-		if !env.call_method(&clipboard_manager, "hasPrimaryClip", "()Z", &[])?.z()? {
-			return Err(Error::ContentNotAvailable);
-		}
+			let clip = env
+				.call_method(
+					clipboard_manager,
+					c"getPrimaryClip",
+					c"()Landroid/content/ClipData;",
+					&[],
+				)?
+				.l()?;
 
-		let clip = env
-			.call_method(clipboard_manager, "getPrimaryClip", "()Landroid/content/ClipData;", &[])?
-			.l()?;
+			if env.call_method(&clip, c"getItemCount", c"()I", &[])?.i()? == 0 {
+				return Err(Error::ContentNotAvailable);
+			}
 
-		if env.call_method(&clip, "getItemCount", "()I", &[])?.i()? == 0 {
-			return Err(Error::ContentNotAvailable);
-		}
+			let item = env
+				.call_method(
+					&clip,
+					c"getItemAt",
+					c"(I)Landroid/content/ClipData$Item;",
+					&[0.into()],
+				)?
+				.l()?;
 
-		let item = env
-			.call_method(&clip, "getItemAt", "(I)Landroid/content/ClipData$Item;", &[0.into()])?
-			.l()?;
+			let char_sequence =
+				env.call_method(item, c"getText", c"()Ljava/lang/CharSequence;", &[])?.l()?;
+			let text = env.cast_local::<JString>(char_sequence)?.to_string();
 
-		let text = env.call_method(item, "getText", "()Ljava/lang/CharSequence;", &[])?.l()?;
-		let text = JString::from(text);
-		let text = env.get_string(&text)?;
-		Ok(text.into())
+			Ok(text)
+		})
+	}
+
+	pub(crate) fn html(self) -> Result<String, Error> {
+		Err(Error::ClipboardNotSupported)
 	}
 
 	pub(crate) fn image(self) -> Result<ImageData<'static>, Error> {
+		Err(Error::ClipboardNotSupported)
+	}
+
+	pub(crate) fn file_list(self) -> Result<Vec<PathBuf>, Error> {
 		Err(Error::ClipboardNotSupported)
 	}
 }
@@ -101,28 +118,26 @@ impl<'clipboard> Set<'clipboard> {
 	}
 
 	pub(crate) fn text(self, text: Cow<'_, str>) -> Result<(), Error> {
-		let vm = self.clipboard.vm()?;
-		let mut env = vm.attach_current_thread()?;
-		let clipboard_manager = self.clipboard.clipboard_manager(&mut env)?;
+		function(|env, clipboard_manager| {
+			let label = env.new_string(c"label")?;
+			let text = env.new_string(JNIString::from(text))?;
 
-		let label = env.new_string("label")?;
-		let text = env.new_string(text)?;
+			let clip_data = env.call_static_method(
+				c"android/content/ClipData",
+				c"newPlainText",
+				c"(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Landroid/content/ClipData;",
+				&[(&label).into(), (&text).into()],
+			)?;
 
-		let clip_data = env.call_static_method(
-			"android/content/ClipData",
-			"newPlainText",
-			"(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Landroid/content/ClipData;",
-			&[(&label).into(), (&text).into()],
-		)?;
+			env.call_method(
+				clipboard_manager,
+				c"setPrimaryClip",
+				c"(Landroid/content/ClipData;)V",
+				&[(&clip_data).into()],
+			)?;
 
-		env.call_method(
-			clipboard_manager,
-			"setPrimaryClip",
-			"(Landroid/content/ClipData;)V",
-			&[(&clip_data).into()],
-		)?;
-
-		Ok(())
+			Ok(())
+		})
 	}
 
 	pub(crate) fn html(self, _: Cow<'_, str>, _: Option<Cow<'_, str>>) -> Result<(), Error> {
@@ -131,6 +146,10 @@ impl<'clipboard> Set<'clipboard> {
 
 	#[cfg(feature = "image-data")]
 	pub(crate) fn image(self, _: ImageData) -> Result<(), Error> {
+		Err(Error::ClipboardNotSupported)
+	}
+
+	pub(crate) fn file_list(self, _: &[impl AsRef<Path>]) -> Result<(), Error> {
 		Err(Error::ClipboardNotSupported)
 	}
 }
@@ -145,12 +164,9 @@ impl<'clipboard> Clear<'clipboard> {
 	}
 
 	pub(crate) fn clear(self) -> Result<(), Error> {
-		let vm = self.clipboard.vm()?;
-		let mut env = vm.attach_current_thread()?;
-		let clipboard_manager = self.clipboard.clipboard_manager(&mut env)?;
-
-		env.call_method(clipboard_manager, "clearPrimaryClip", "()V", &[])?;
-
-		Ok(())
+		function(|env, clipboard_manager| {
+			env.call_method(clipboard_manager, c"clearPrimaryClip", c"()V", &[])?;
+			Ok(())
+		})
 	}
 }
